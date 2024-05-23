ee747ee6e064b653f3c38fa402b5415804492426
Oğuz Özcan <oguz.ozcan@vennyx.com>
2024-05-23 05:48:30 +0300

    0.1.7 released

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,8 @@
-import packageJson from "../package.json";
-
 export default class Constants {
   static readonly RANDOM_STRING_SIZE = 8;
   static readonly RANDOM_HEADER_NAME = "x-iyzi-rnd";
   static readonly CLIENT_VERSION = "x-iyzi-client-version";
-  static readonly CLIENT_VERSION_VALUE = packageJson.name + "-" + packageJson.version;
+  static readonly CLIENT_VERSION_VALUE = "iyzico-iyzipay-client-1.0.7";
   static readonly IYZI_WS_HEADER_NAME = "IYZWS";
   static readonly IYZI_WS_HEADER_NAME_V2 = "IYZWSv2";
 
