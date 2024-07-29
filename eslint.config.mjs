8a38a0ad50f7a4cb727bac8c85ea5c9eaba17f12
Oğuz Özcan <oguz.ozcan@vennyx.com>
2024-07-30 00:04:31 +0300

    0.1.17 released - Changed to CommonJs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,29 @@
+/* eslint-disable */
+
 // @ts-check
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        project: "./tsconfig.eslint.json",
-      },
+const eslintConfig = tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                project: "./tsconfig.eslint.json",
+            },
+        },
     },
-  },
-  {
-    ignores: ["dist/**/*", "node_modules/**/*"],
-  },
-  {
-    files: ["**/*.js"],
-    ...tseslint.configs.disableTypeChecked,
-  },
-  prettierConfig,
+    {
+        ignores: ["dist/**/*", "node_modules/**/*"],
+    },
+    {
+        files: ["**/*.js"],
+        ...tseslint.configs.disableTypeChecked,
+    },
+    prettierConfig,
 );
+
+export default eslintConfig;
